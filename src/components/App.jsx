4dbfb4f7f7678167a6042dc86d35bf2096317f5c
magtesyan <magtesyan@inbox.ru>
2021-05-20 18:03:32 +0300

    add preloader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import history from "./../history";
-import { Switch, Route, Router } from "react-router-dom";
+import { Switch, Route, Router } from 'react-router-dom';
 import Main from './Main.jsx';
+import Data from './Data.jsx';
+import { DATA_TYPES } from './../const.js';
 
 const App =() => {
-
   return (
     <Router history={history}>
       <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
+        <Route exact path="/"><Main /></Route>
+        <Route exact path="/small"><Data dataType={DATA_TYPES[0]}/></Route>
+        <Route exact path="/big"><Data dataType={DATA_TYPES[1]}/></Route>
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
