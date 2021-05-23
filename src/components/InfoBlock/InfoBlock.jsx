2f6795191c1fa1c93e30669123b84cb4433d27b0
magtesyan <magtesyan@inbox.ru>
2021-05-23 22:47:17 +0300

    fix: no info for added row

--- src/components/InfoBlock/InfoBlock.jsx
+++ src/components/InfoBlock/InfoBlock.jsx
@@ -5,11 +5,15 @@ const InfoBlock = (props) => {
   return (
     <div className={classes.div}>
       <div>Выбран пользователь: <b>{props.person.firstName} {props.person.lastName}</b></div>
-      <textarea className={classes.textarea} value={props.person.description} readOnly />
-      <div>Адрес проживания: <b>{props.person.address.streetAddress}</b></div>
-      <div>Город: <b>{props.person.address.city}</b></div>
-      <div>Провинция/штат: <b>{props.person.address.state}</b></div>
-      <div>Индекс: <b>{props.person.address.zip}</b></div>
+      {props.person.description && <textarea className={classes.textarea} value={props.person.description} readOnly />}
+      {props.person.address && 
+        <div>
+          <div>Адрес проживания: <b>{props.person.address.streetAddress}</b></div>
+          <div>Город: <b>{props.person.address.city}</b></div>
+          <div>Провинция/штат: <b>{props.person.address.state}</b></div>
+          <div>Индекс: <b>{props.person.address.zip}</b></div>
+        </div>
+      }
     </div>
   );
 };
